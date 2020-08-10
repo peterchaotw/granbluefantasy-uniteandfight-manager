5d2655109c3d8312580cb73b3a5c3f2525fa6f13
peterchaotw <peter.chao.forjob@gmail.com>
2020-08-10 15:51:08 +0900

    read file to search

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from gbfteamraid import gbfteamraid
 from data_handler import data_handler
 
+def read_member_list():
+	res = []
+	with open('teams.txt', 'r') as file_to_read:
+		while True:
+			lines = file_to_read.readline().replace('\n', '')
+			if not lines:
+				break
+			res.append(lines)
+			pass
+	return res
 
 def main():
+    teams = read_member_list()
     service = gbfteamraid()
-    members1 = service.report_team_members('社畜団￥サービス残業')
-    members2 = service.report_team_members('QK団￥グラブル残業')
-    members = members1 + members2
+    members =[]
+    for team in teams:
+        _members = service.report_team_members(team)
+        members = members+_members
     handler = data_handler(members)
     handler.export_csv_report()
 
